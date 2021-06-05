0033924f22261c615d7fd3b6425ba58cec8aa0c3
KataTech <kmh9@rice.edu>
2021-06-05 14:35:26 -0500

    updated with Kai info

--- src/mock/data.js
+++ src/mock/data.js
@@ -9,57 +9,60 @@ export const headData = {
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  title: 'Hi, I am ',
+  name: 'Kai Hung',
+  subtitle: 'CS, STAT, and Living Life at Rice',
+  cta: 'About Me',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  paragraphOne:
+    'Welcome! I am a kid who’s passionate about technology, data science, Marvel, Pokemon, League of Legends, basketball, helping others, and cracking jokes. As someone who immigrated to the US from Hong Kong a decade ago, I love to help students from underrepresented immigrant background to navigate through their needs on foreign soil. I am a firm believer in positive mental attitude, aspirer to uphold my own integrity as Captain America does, and a jokester who couldn’t bare to live a moment of his life in boredom. ',
+  paragraphTwo:
+    'As a first generation student studying Computer Science and Statistics at Rice University, I am always looking for opportunities to grow, engage with exciting new technology, and build projects that actually benefit the community that has provided me with so much support. Currently, I am working as a research intern at MD Anderson analyzing big Data to extract insights for cancer treatment, learning web development to contribute to Rice Carpool, a ride-sharing web application made for Rice students, and expanding my knowledge by reading Intro to Statistical Learning. ',
+  paragraphThree: 'Keep scrolling to see more!',
+  resume: 'https://drive.google.com/file/d/1KgWhrBLBGPJr9HTmZSXgAz5BswodzbBU/view?usp=sharing', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'Rice Carpool.png',
+    title: 'Rice Carpool',
+    info: 'Rice Carpool is a ride-sharing platform made by Rice Apps to allow students to connect with those who also want to share a ride to popular locations in Houston such as Rice Village, Galleria, Airports conveniently.',
+    info2:
+      'I am currently learning web development tools such as HTML, CSS, Javascript (React JS) to contribute to this project. More updates will come soon.',
+    url: 'https://carpool.riceapps.org/',
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'Basketball.jpeg',
+    title: 'Jocks Box',
+    info: "Jocks Box is my team's project for Rice Datasci Club' Data Education and Exploration Project (DEEP) of 2020. The project entails identifying key factors that contribute to the outcome of an NBA game, splitting our dataset into training vs. testing sets, constructing a predictive model based on these factors, and evaluating the performance of our models via statistical metrics.",
+    info2:
+      "I was in charge of implementing the linear model, outputing the coefficients and predicted win rates, demonstrating variable coefficient changes over time through visualization, writing presentation-friendly explanations in the Jupyter notebook through the markup tool, and optimizing our project's transferability with new season data by implementing it as a function. Overall, our project predicted the win rates with an average root mean squared error of 6.56%.",
+    url: 'https://github.com/KataTech/NBA-Analysis',
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'Firefighter.jpg',
+    title: 'Firefighter Fight What?',
+    info: 'This is a data science project my teammates and I started given our interest for the types of problems that firefighters and emergency responders combat. We obtained a dataset from the San Francisco Fire Department and performed data analysis in hopes of determining the most frequent incident types, seasonal patterns, as well as regional patterns based on minority distributions. ',
+    info2:
+      'Personally, I created a database from the massive CSV, extracted the data through SQL, performed data cleaning, data visualization, and implemented various statistical testings to validates trends that we observed from the data (ANOVA, Multiple Linear Regression) related to common call types, change in response time across regions with varying minority distribution.',
+    url: 'https://github.com/KataTech/Firefighters-Fight-What-/blob/main/FireAnalysis_Final.pdf',
+    repo: 'https://github.com/KataTech/Firefighters-Fight-What-/blob/main/FireAnalysis_Final.Rmd',
   },
 ];
 
 // CONTACT DATA
 export const contactData = {
-  cta: '',
-  btn: '',
-  email: '',
+  cta: 'Want to connect with me? Awesome!',
+  btn: 'Shoot me an Email',
+  email: 'kmh9@rice.edu',
 };
 
 // FOOTER DATA
@@ -67,23 +70,13 @@ export const footerData = {
   networks: [
     {
       id: nanoid(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
-    },
-    {
-      id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/kai-hung-425a3918b/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/KataTech',
     },
   ],
 };
